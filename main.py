2f21d4d2739e9978257076e87c97b939079fb56b
nobody-r <coparei7@gmail.com>
2022-05-20 09:39:12 +0200

    2ez

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ import lxml
 from flask import Flask, render_template, request, send_file, redirect, session, g
 from json import dumps
 
-'''
+"""
 @app.route('/tool2', methods=['post', 'get'])
 def cap():
     cap = request.form.get('cap')
     return render_template('tool2.html', cap=cap)
-    '''
+    """
 prezzo = 10.5
 j = 0
 lista = None
 soup = None
-headers={
+headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
-  #  "Accept-Language": "en"
+    #  "Accept-Language": "en"
 }
+
 
 def stazioni(cap1):
     global soup
@@ -27,10 +28,8 @@ def stazioni(cap1):
     r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.text, "lxml")
     stazioni = soup.find("div", "poilist-result-count")
-    NrStazioni= stazioni
-    lista ="".join(map(str,soup.findAll("ul", "poilist clearfx")))
-
-
+    NrStazioni = stazioni
+    lista = "".join(map(str, soup.findAll("ul", "poilist clearfx")))
 
 
 def fungasolio():
@@ -38,8 +37,12 @@ def fungasolio():
     prezzo_gasolio = prezzo
     jgas = j
     for i in gasolio:
-        if (prezzo_gasolio > float(gasolio[jgas].find("span", "poi-item-fuel-value").text)):
-            prezzo_gasolio = float(gasolio[jgas].find("span", "poi-item-fuel-value").text)
+        if prezzo_gasolio > float(
+            gasolio[jgas].find("span", "poi-item-fuel-value").text
+        ):
+            prezzo_gasolio = float(
+                gasolio[jgas].find("span", "poi-item-fuel-value").text
+            )
             posgasolio = jgas
         jgas = jgas + 1
     return prezzo_gasolio
@@ -50,7 +53,7 @@ def funsp():
     prezzo_sp95 = prezzo
     jsp = j
     for i in sp95:
-        if (prezzo_sp95 > float(sp95[jsp].find("span", "poi-item-fuel-value").text)):
+        if prezzo_sp95 > float(sp95[jsp].find("span", "poi-item-fuel-value").text):
             prezzo_sp95 = float(sp95[jsp].find("span", "poi-item-fuel-value").text)
             possp95 = jsp
         jsp = jsp + 1
@@ -62,7 +65,7 @@ def fungpl():
     prezzo_gpl = prezzo
     jgpl = j
     for i in gpl:
-        if (prezzo_gpl > float(gpl[jgpl].find("span", "poi-item-fuel-value").text)):
+        if prezzo_gpl > float(gpl[jgpl].find("span", "poi-item-fuel-value").text):
             prezzo_gpl = float(gpl[jgpl].find("span", "poi-item-fuel-value").text)
             posgpl = jgpl
         jgpl = jgpl + 1
@@ -74,49 +77,44 @@ def funmet():
     prezzo_metano = prezzo
     jmet = j
     for i in metano:
-        if (prezzo_metano > float(metano[jmet].find("span", "poi-item-fuel-value").text)):
+        if prezzo_metano > float(metano[jmet].find("span", "poi-item-fuel-value").text):
             prezzo_metano = float(metano[jmet].find("span", "poi-item-fuel-value").text)
             posmetano = j
         jmet = jmet + 1
     return prezzo_metano
 
 
-
 app = Flask(__name__)
 app.secret_key = "key"
 
-@app.route('/', methods=["POST", "GET"])
+
+@app.route("/", methods=["POST", "GET"])
 def index():
     return render_template("index.html")
 
-@app.route('/about', methods=["POST", "GET"])
+
+@app.route("/about", methods=["POST", "GET"])
 def about():
     return render_template("about.html")
 
-@app.route('/tool2', methods=["POST", "GET"])
+
+@app.route("/tool2", methods=["POST", "GET"])
 def tool2():
     global lista
-    cap = '70024'
-    stazioni(cap)
-    print(lista)
-    return render_template("tool2.html",
-                           stazioni=str(lista),
-                           cap = cap)
+    return render_template("tool2.html")
 
 
-@app.route('/tool3', methods=["POST", "GET"])
-def tool2():
+@app.route("/tool3", methods=["POST", "GET"])
+def tool3():
     global lista
-    cap = request.form['cap']
+    cap = request.form["cap"]
     stazioni(cap)
-    print(lista)
-    return render_template("tool2.html",
-                           stazioni=str(lista),
-                           cap = cap)
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    return render_template("tool3.html", stazioni=str(lista), cap=cap)
 
 
-#sostituire len(prezzo1) a stazioni      //print(prezzo1[0])
-#print(prezzo1[0].find("span", "poi-item-fuel-value").text)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
+
+
+# sostituire len(prezzo1) a stazioni      //print(prezzo1[0])
+# print(prezzo1[0].find("span", "poi-item-fuel-value").text)
